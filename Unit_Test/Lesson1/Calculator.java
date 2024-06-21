8b48a9433fd7a6aa105f0e59d5c101d890a628dc
AurRazex <AlekSevrus@gmail.com>
2024-06-21 15:19:31 +0300

    1

--- Unit_Test/Lesson1/Calculator.java
+++ Unit_Test/Lesson1/Calculator.java
@@ -9,11 +9,15 @@ public class Calculator {
 
         if (purchaseAmount <= 0){
             throw new ArithmeticException("The purchase amount cannot be less than or equal to zero");
+
         } else if (discountAmount < 0){
             throw new ArithmeticException("The discount amount cannot be less than zero");
+
         } else if (discountAmount > 100){
             throw new ArithmeticException("The discount amount cannot be higher than to 100");
+
         }
+
         amountTemp = purchaseAmount - ((purchaseAmount / 100) * discountAmount);
         discountedAmount = Math.round(amountTemp * 100D) / 100D;
         return discountedAmount;
--- Unit_Test/Lesson1/CalculatorTest.java
+++ Unit_Test/Lesson1/CalculatorTest.java
@@ -12,35 +12,40 @@ public class CalculatorTest {
         testCalculateDiscountByDiscountAmountMoreThanOneHundredPercent();
     }
 
-    // Проверка базового функционала метода
+
     public static void testCalculateDiscountByBasicFunctionality(){
+
         assertThat(Calculator.calculateDiscount(122.33, 3)).isEqualTo(118.66);
         System.out.println("Сумма к оплате с учетом скидки " + Calculator.calculateDiscount(122.33,3));
     }
 
-    // Проверка с отрицательным значением PurchaseAmount
+
     public static void testCalculateDiscountByPurchaseAmountLessThanZero(){
+
         assertThatThrownBy(() -> Calculator.calculateDiscount(-1.00, 25))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage("The purchase amount cannot be less than or equal to zero");
     }
 
-    // Проверка со значением PurchaseAmount=0
+
     public static void testCalculateDiscountByPurchaseAmountEqualToZeroZero(){
+
         assertThatThrownBy(() -> Calculator.calculateDiscount(0, 25))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage("The purchase amount cannot be less than or equal to zero");
     }
 
-    // Проверка с отрицательным значением DiscountAmount
+
     public static void testCalculateDiscountByDiscountAmountLessThanZero(){
+
         assertThatThrownBy(() -> Calculator.calculateDiscount(100.00, -25))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage("The discount amount cannot be less than zero");
     }
 
-    // Проверка со значением DiscountAmount больше 100
+
     public static void testCalculateDiscountByDiscountAmountMoreThanOneHundredPercent(){
+
         assertThatThrownBy(() -> Calculator.calculateDiscount(100.00, 101))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage("The discount amount cannot be higher than to 100");
